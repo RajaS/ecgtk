3181c3ae54c8853eaf358c48fc258e2fc00c2ae9
Raja S <rajajs@gmail.com>
2009-12-19 18:34:10 +0530

    rewriting wfdbtools as a collection of functions

--- wfdbtools.py
+++ wfdbtools.py
@@ -1,117 +1,112 @@
 #!/usr/bin/env python
 
 """
-Import ECGs in 'format 212' which is the most common format
-for ECG data in Physionet.
-Only records with 2 channels are supported"""
-
-# Based on rddata.m for matlab written by Robert Tratnig
-# available here -
-# http://physionet.org/physiotools/matlab/rddata.m
-
+Provide some of the wfdb IO tools in pure python.
+I will try to keep names and usage similar to the
+original wfdb applications for simplicity.
+"""
 # Author: Raja Selvaraj <rajajs@gmail.com>
 # Written in December 2009
 # License: GPL
+
+# rdsamp based on rddata.m for matlab written by Robert Tratnig
+# available at http://physionet.org/physiotools/matlab/rddata.m
 
 from __future__ import division
 import os
 import numpy
 import pylab
 
-class Record212():
-    """WFDB format 212 record"""
-    def __init__(self, datafile, samplestart=0, sampleend=0):
-        """
-        datafile is the path to the dat file
-        samplestart and sampleend are in seconds
-	"""
-        self.datafile = datafile
-        recordname = os.path.splitext(datafile)[0]
-        self.headerfile = recordname + '.hea'
-        self.atrfile = recordname + '.atr'
-        self.samplestart = samplestart 
-        self.sampleend = sampleend
-        # store header information for each signal
-        self.gain = []
-        self.bitres = []
-        self.zerovalue = []
-        self.firstvalue = []
-        # Load header information
-        self.signals = []
-        self.load_header()
-        # unless samplestart and end are valid,
-        # read all the data
-        if not self.valid_start_end():
-            self.samplestart = 0
-            self.sampleend = self.samp_ct
+def rdsamp(record, start=0, end=-1, interval=-1, timecol=True):
+    """
+    Read signals from the specified wfdb record.
+    **Only supports format 212 records with 2 signals**
+    record - name of record without extension
+    start - time to begin in seconds, default 0
+    end - time to end in seconds, defaults to end of record
+    interval - interval to read.
+    If both interval and end are given, earlier limit is used.
+    If timecol is True (default), elapsed time in seconds is first column.
+    If timecol is False, sample number is given instead of elapsed time.
+    """
+    # read the header file
+    (samp_freq, samp_count, gains,
+     zerovalues, firstvalues) = _read_header(record)
+    # establish start and end in samples
+    start, end = _get_read_limits(start, end, interval, samp_freq, samp_count)
+    # read the data
+    data = _read_data(record)
 
-        # read the binary data
-        self.read_data()
+def _read_header(record):
+    """Read the headerfile for the record"""
+    headerfile = record + '.hea'
+    gains = []; zerovalues = []; firstvalues = []
+    fid = open(headerfile, 'r')
+    firstline = fid.readline()
+    recordname, signal_count, samp_freq, samp_count = firstline.split()
+    # Number of signal must be exactly 2
+    if int(signal_count) != 2:
+        raise ValueError, 'Input data must have exactly 2 signals'
+    
+    # load signal attributes
+    # TODO: more robust parsing 
+    for i in range(signal_count):
+        (filename,
+         format_name,                 # should be 212
+         gain,                        # Integers per mV
+         bitres,                      # Bit Resolution
+         zerovalue,                   # value of zero point
+         firstvalue,                  # First value of signal
+         dummy,
+         dummy,
+         signal_name
+         ) = fid.readline().rstrip('\n').split()
 
-        # plot data
-        self.plot_data()
+        if format_name != '212':
+            raise ValueError, 'Not in format 212'
         
-    def valid_start_end(self):
-        """are samplestart and end inputs valid"""
-        start, end = self.samplestart, self.sampleend
-        if start < 0 or end < 0:
-            return False
-        elif end <= start:
-            return False
-        else:
-            return True
-        
-    def load_header(self):
-        """Load the header data"""
-        fi = open(self.headerfile, 'r')
-        firstline = fi.readline()
-        recordname, signal_ct, samp_rate, samp_ct = firstline.split()
+        gains.append(int(gain))
+        zerovalues.append(int(zerovalue))
+        firstvalues.append(int(firstvalue))
+    fid.close()
+    return (samp_freq, samp_count, gains,
+            zerovalues, firstvalues)
 
-        self.signal_ct = int(signal_ct)
-        self.samp_rate = int(samp_rate)
-        self.samp_ct = int(samp_ct)
+def _get_read_limits(start, end, interval, samp_freq, samp_count):
+    """
+    Given start time, end time and interval
+    for reading data, determine limits to use.
+    samp_count is number of samples in record.
+    end of -1 means end of record.
+    If both end and interval are given, choose
+    earlier limit of two.
+    """
+    start *= samp_freq
+    end *= samp_freq
+    
+    if start < 0:         # If start is negative, start at 0
+        start = 0
+    if end < 0:           # if end is negative, use end of record
+        end = samp_count
+    if end < start:       # if end is before start, swap them
+        start, end = end, start
+    end = min(end, start + interval*samp_freq, samp_count) # use earlier end
+    return start, end
 
-        # only data with 2 signals
-        if self.signal_ct != 2:
-            raise ValueError, 'Input data must have exactly 2 signals'
-        # change samplestart and end to samples
-        self.samplestart *= self.samp_rate
-        self.sampleend *= self.samp_rate
-        
-        # load signal attributes
-        # TODO: more robust parsing 
-        for i in range(self.signal_ct):
-            (filename,
-             format_name,                 # should be 212
-             gain,                        # Integers per mV
-             bitres,                      # Bit Resolution
-             zerovalue,                   # value of zero point
-             firstvalue,                  # First value of signal
-             dummy,
-             dummy,
-             signal_name
-             ) = fi.readline().rstrip('\n').split()
-
-            if format_name != '212':
-                raise ValueError, 'Not in format 212'
-            self.gain.append(int(gain))
-            self.bitres.append(int(bitres))
-            self.zerovalue.append(int(zerovalue))
-            self.firstvalue.append(int(firstvalue))
-        fi.close()
             
-    def read_data(self):
-        """Read the binary data for each signal"""
-        fi = open(self.datafile, 'rb')
-        # read into an array with 3 bytes in each row
-        arr = numpy.fromstring(fi.read(3*self.sampleend),
-                    dtype=numpy.uint8).reshape((self.sampleend, 3))
-        # bit operations to get the 12-bit data
-        second_col = arr[:, 1].astype('int')
-        bytes1 = second_col & 15 # bytes belonging to first sample
-        bytes2 = second_col >> 4 # belongs to second sample
-        sign1 = (second_col & 8) << 9 # sign bit for first sample
-        sign2 = (second_col & 128) << 5 # sign bit for second sample
+def _read_data(record, start, end, samp_freq):
+    """Read the binary data for each signal"""
+    datfile = record + '.dat'
+    fid = open(datfile, 'rb')
+    # read into an array with 3 bytes in each row
+    arr = numpy.fromstring(fid.read(3*end),
+                dtype=numpy.uint8).reshape((end, 3))
+    # bit operations to get the 12-bit data
+    second_col = arr[:, 1].astype('int')
+    bytes1 = second_col & 15 # bytes belonging to first sample
+    bytes2 = second_col >> 4 # belongs to second sample
+    sign1 = (second_col & 8) << 9 # sign bit for first sample
+    sign2 = (second_col & 128) << 5 # sign bit for second sample
         
         self.data = numpy.zeros((self.sampleend, 2), dtype=numpy.uint16)
         self.data[:, 0] = (bytes1 << 8) + arr[:, 0] - sign1
