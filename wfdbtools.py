0d6daf89417399548f5c89c45c8423dde60e1818
Raja S <rajajs@gmail.com>
2009-12-20 20:42:30 +0530

    rdann completed. Both rdann and rdsamp now output time in both samples and ms. Time in output is always time from beginning of the record

--- wfdbtools.py
+++ wfdbtools.py
@@ -17,7 +17,7 @@ import warnings
 import numpy
 import pylab
 
-def rdsamp(record, start=0, end=-1, interval=-1, timecol=True):
+def rdsamp(record, start=0, end=-1, interval=-1):
     """
     Read signals from the specified wfdb record.
     **Only supports format 212 records with 2 signals**
@@ -26,8 +26,8 @@ def rdsamp(record, start=0, end=-1, interval=-1, timecol=True):
     end - time to end in seconds, defaults to end of record
     interval - interval to read.
     If both interval and end are given, earlier limit is used.
-    If timecol is True (default), elapsed time in seconds is first column.
-    If timecol is False, sample number is given instead of elapsed time.
+    Elapsed time in samples is in first column of output.
+    Elapsed time in milliseconds is second column.
     """
     # TODO: implement ability to read 
     # read the header file
@@ -37,18 +37,26 @@ def rdsamp(record, start=0, end=-1, interval=-1, timecol=True):
     start, end = _get_read_limits(start, end, interval, samp_freq, samp_count)
     # read the data
     data = _read_data(record, start, end, samp_freq,
-                      zerovalues, firstvalues, gains, timecol)
+                      zerovalues, firstvalues, gains)
     return data
 
 
-def rdann(record, annotator, start=0, end=-1, type=None, subtype=None):
+def rdann(record, annotator, start=0, end=-1):
     """Read the annotation for given record by the annotator.
     record is path to record, no extension.
     annotator is the name of annotator, eg. 'atr'.
-    Optional start and end are in seconds
+    Optional start, end and interval are in seconds
     and specify interval when annot should be read.
-    type and subtype allow specific annotations to be selected
+    If both end and interval are given, the earlier
+    of the two limits is chosen.
+    Array that is returned has time in samples in first column,
+    time in ms in second column
+    and annotations code in third column
     """
+    # get header data
+    (samp_freq, samp_count, gains,
+                   zerovalues, firstvalues) = _read_header(record)
+    
     annfile = record + '.' + annotator
     fid = open(annfile, 'rb')
     arr = numpy.fromstring(fid.read(), dtype = numpy.uint8).reshape((-1, 2))
@@ -58,32 +66,41 @@ def rdann(record, annotator, start=0, end=-1, type=None, subtype=None):
     annot = []
     annot_time = []
     i = 0
-    while i < 30:
-       anntype = arr[i, 1] >> 2
-       if anntype == 59:
-           annot.append(arr[i+3, 1] >> 2)
-           annot_time.append(arr[i+2, 0] + (arr[i+2, 1] << 8) +
-                             (arr[i+1, 0] << 16) + arr[i+1, 1] << 24)
-           i += 3
-       elif anntype in [60, 61, 62, 63]:
-           hilfe = arr[i, 0] + ((arr[i, 1] & 3) << 8)
-           hilfe += hilfe % 2
-           print 'hilfe', hilfe
-           i += hilfe / 2
-       else:
-           annot_time.append(arr[i, 0] + ((arr[i, 1] & 3) << 8))
-           annot.append(arr[i, 1] >> 2)
-       i += 1
-
+    while i < rows:
+        anntype = arr[i, 1] >> 2
+        if anntype == 59:
+            annot.append(arr[i+3, 1] >> 2)
+            annot_time.append(arr[i+2, 0] + (arr[i+2, 1] << 8) +
+                              (arr[i+1, 0] << 16) + arr[i+1, 1] << 24)
+            i += 3
+        elif anntype in [60, 61, 62, 63]:
+            hilfe = arr[i, 0] + ((arr[i, 1] & 3) << 8)
+            hilfe += hilfe % 2
+            i += hilfe / 2
+        else:
+            annot_time.append(arr[i, 0] + ((arr[i, 1] & 3) << 8))
+            annot.append(arr[i, 1] >> 2)
+        i += 1
+    # annot_time should be total elapsed samples
     annot_time = numpy.cumsum(annot_time)
-
+    annot_time_ms = annot_time * 1000 // samp_freq
+    # limit to requested interval
+    start, end = _get_read_limits(start, end, -1, samp_freq, samp_count)
+    ann = numpy.array([annot_time, annot_time_ms, annot]).transpose()
+    # filter by annot_time in interval
+    ann =  ann[start <= ann[:, 0]]
+    ann = ann[ann[:, 0] <= end]
+    return ann
+    
 def plot_data(data, ann=None):
     """Plot the signals"""
-    time = data[:, 0]
+    time = data[:, 1] # used data[:, 2] to use sample no.
     pylab.subplot(211)
-    pylab.plot(time, data[:, 1], 'k')
-    pylab.subplot(212)
     pylab.plot(time, data[:, 2], 'k')
+    pylab.plot(ann[:, 1], data[ann[:, 2], 2], 'xr')
+    pylab.subplot(212)
+    pylab.plot(time, data[:, 3], 'k')
+    pylab.plot(ann[:, 1], data[ann[:, 2], 3], 'xr')
     pylab.show()
 
 def _read_header(record):
@@ -146,7 +163,7 @@ def _get_read_limits(start, end, interval, samp_freq, samp_count):
     return start, end
             
 def _read_data(record, start, end, samp_freq,
-               zerovalues, firstvalues, gains, timecol):
+               zerovalues, firstvalues, gains):
     """Read the binary data for each signal"""
     datfile = record + '.dat'
     samp_to_read = end - start
@@ -156,8 +173,8 @@ def _read_data(record, start, end, samp_freq,
     data = _arr_to_data(numpy.fromstring(fid.read(3),
                         dtype=numpy.uint8).reshape(1,3))
     fid.close()
-    if [data[0, 1], data[0, 2]] != firstvalues:
-        warnings.warn('First value read from dat file does not match value in header')
+    if [data[0, 2], data[0, 3]] != firstvalues:
+        warnings.warn('First value from dat file does not match value in header')
     
     # read into an array with 3 bytes in each row
     fid = open(datfile, 'rb')
@@ -168,13 +185,11 @@ def _read_data(record, start, end, samp_freq,
     data = _arr_to_data(arr)
 
     # adjust zerovalue and gain
-    data[:, 1] -= zerovalues[0] / gains[0]
-    data[:, 2] -= zerovalues[1] / gains[1]
+    data[:, 2] -= zerovalues[0] / gains[0]
+    data[:, 3] -= zerovalues[1] / gains[1]
     time = numpy.arange(samp_to_read) *1000 // samp_freq # in ms
-    if timecol:
-        data[:, 0] = time
-    else:
-        data[:, 0] = numpy.arange(end)
+    data[:, 0] = numpy.arange(start, end) + start
+    data[:, 1] = time + (start * 1000 // samp_freq)
     return data
 
 def _arr_to_data(arr):
@@ -186,18 +201,17 @@ def _arr_to_data(arr):
     bytes2 = second_col >> 4 # belongs to second sample
     sign1 = (second_col & 8) << 9 # sign bit for first sample
     sign2 = (second_col & 128) << 5 # sign bit for second sample
-    # data has columns - time, signal1 and signal2
-    data = numpy.zeros((arr.shape[0], 3), dtype='int')
-    data[:, 1] = (bytes1 << 8) + arr[:, 0] - sign1
-    data[:, 2] = (bytes2 << 8) + arr[:, 2] - sign2
+    # data has columns - samples, time(ms), signal1 and signal2
+    data = numpy.zeros((arr.shape[0], 4), dtype='int')
+    data[:, 2] = (bytes1 << 8) + arr[:, 0] - sign1
+    data[:, 3] = (bytes2 << 8) + arr[:, 2] - sign2
     return data
 
-
-
 def test():
-    #data = rdsamp('/data/Dropbox/programming/ECGtk/samples/format212/100')
-    #plot_data(data)
-    rdann('/data/Dropbox/programming/ECGtk/samples/format212/100', 'atr')
+    data = rdsamp('/data/Dropbox/programming/ECGtk/samples/format212/100', 1, 10)
+    ann = rdann('/data/Dropbox/programming/ECGtk/samples/format212/100', 'atr', 1, 10)
+
+    plot_data(data, ann)
     
     
 if __name__ == '__main__':
